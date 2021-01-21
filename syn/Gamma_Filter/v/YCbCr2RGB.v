747a186985698e13dff401d1adfca517fd1d0203
Cole Maxwell <cole.h.maxwell@gmail.com>
2021-01-20 19:44:59 -0500

    update references

--- syn/Gamma_Filter/v/YCbCr2RGB.v
+++ syn/Gamma_Filter/v/YCbCr2RGB.v
@@ -1,45 +1,8 @@
 // ============================================================================
-// Copyright (c) 2012 by Terasic Technologies Inc.
-// ============================================================================
-//
-// Permission:
-//
-//   Terasic grants permission to use and modify this code for use
-//   in synthesis for all Terasic Development Boards and Altera Development 
-//   Kits made by Terasic.  Other use of this code, including the selling 
-//   ,duplication, or modification of any portion is strictly prohibited.
-//
-// Disclaimer:
-//
-//   This VHDL/Verilog or C/C++ source code is intended as a design reference
-//   which illustrates how these types of functions can be implemented.
-//   It is the user's responsibility to verify their design for
-//   consistency and functionality through the use of formal
-//   verification methods.  Terasic provides no warranty regarding the use 
-//   or functionality of this code.
-//
-// ============================================================================
-//           
-//  Terasic Technologies Inc
-//  9F., No.176, Sec.2, Gongdao 5th Rd, East Dist, Hsinchu City, 30070. Taiwan
-//
-//
-//
-//                     web: http://www.terasic.com/
-//                     email: support@terasic.com
-//
-// ============================================================================
 //
 // Major Functions:	YCbCr to RGB Color Doamin Converter. 
 //					( 10 Bits Resolution )
 //
-// ============================================================================
-//
-// Revision History :
-// ============================================================================
-//   Ver  :| Author            :| Mod. Date   :| Changes Made:
-//   V1.0 :| Johnny Chen       :| 05/09/05    :|      Initial Revision
-//   V2.0 :| Peli   Li         :| 04/19/2010  :|      revised the megacore instance
 // ============================================================================
 
 module YCbCr2RGB (	SWITCH,Red,Green,Blue,oDVAL,
